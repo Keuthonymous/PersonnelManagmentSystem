665cd202f08e049534de2982ab05744fa3472ee1
Keuthonymous <mikie.daughtrey@gmail.com>
2017-07-26 14:47:37 +0200

    Changed _LoginPartial to go to controllers of respective logged in user role. For Example if(User.IsInRole("Admin")) { (Action link to amin controller index) }. Also started Admin control chain.

---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Repositories/AdminRepository.cs
@@ -0,0 +1,24 @@
+﻿using PersonnelManagmentSystemV1.DataAccess;
+using PersonnelManagmentSystemV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonnelManagmentSystemV1.Repositories
+{
+    public class AdminRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<ApplicationUser> GetAllUsers()
+        {
+            return db.Users;
+        }
+
+        public ApplicationUser Find(string id)
+        {
+            return db.Users.Where(u => u.Id == id).First();
+        }
+    }
+}
