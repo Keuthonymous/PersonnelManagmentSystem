f59ca8b075702688340eeb7fa08729a668376f14
name <wille.multi@live.se>
2017-07-28 10:33:52 +0200

    Boss can view departments at Boss/Index

---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Controllers/BossController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PersonnelManagmentSystemV1.DataAccess;
+using PersonnelManagmentSystemV1.Models;
+using PersonnelManagmentSystemV1.Repositories;
+
+namespace PersonnelManagmentSystemV1.Controllers
+{
+    [Authorize(Roles="Boss")]
+    public class BossController : Controller
+    {
+        private DepartmentRepository db = new DepartmentRepository();
+
+        // GET: Boss
+        public ActionResult Index()
+        {
+            
+            return View(db.Departments());
+        }
+
+        // GET: Boss/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Department(id.Value);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Repositories/DepartmentRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PersonnelManagmentSystemV1.DataAccess;
+using PersonnelManagmentSystemV1.Models;
+
+namespace PersonnelManagmentSystemV1.Repositories
+{
+    public class DepartmentRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<Department> Departments()
+        {
+            return db.Departments;
+        }
+
+        public Department Department(int id)
+        {
+            return db.Departments.SingleOrDefault(d => d.ID == id);
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        // This code added to correctly implement the disposable pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
