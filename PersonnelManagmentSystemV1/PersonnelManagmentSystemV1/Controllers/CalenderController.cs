73fe02e8cef88ee1dc16f5e94e633ca17d09b90e
Jocke <jocke@ny-flik.se>
2017-08-03 10:02:44 +0200

    Calender Create

---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Controllers/CalenderController.cs
@@ -0,0 +1,56 @@
+﻿using PersonnelManagmentSystemV1.Models;
+using PersonnelManagmentSystemV1.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using PersonnelManagmentSystemV1.DataAccess;
+
+namespace PersonnelManagmentSystemV1.Controllers
+{
+    public class CalenderController : Controller
+    {
+        private CalenderRepository calrepo = new CalenderRepository();
+
+
+        // GET: Calender
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DepartmentRepository depRepo = new DepartmentRepository();
+
+            ViewBag.DepartmentList = depRepo.Departments().Select(dep => new SelectListItem() { Text = dep.ID.ToString() + " | " + dep.Name, Value = dep.ID.ToString() });
+
+            var calenderVM = new CalenderVM
+            {
+                StartDate = DateTime.Today,
+                StartTime = DateTime.Now,
+                EndDate = DateTime.Today,
+                EndTime = DateTime.Now
+            };
+
+            return View(calenderVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "ID,StartDate,StartTime,EndDate,EndTime,DepartmentID,CalTitle,CalContent,")] CalenderVM calevntVM)
+        {
+            DateTime calenderstart = calrepo.CalStart(calevntVM.StartDate, calevntVM.StartTime);
+            DateTime calenderend = calrepo.CalEnd(calevntVM.EndDate, calevntVM.EndTime);
+
+            if (ModelState.IsValid)
+            {
+                Calender calevnt = new Calender() { DepartmentID = calevntVM.DepartmentID, CalenderStart = calenderstart, CalenderEnd = calenderend, CalTitle = calevntVM.CalTitle, CalContent = calevntVM.CalContent };
+
+                calrepo.AddCalender(calevnt);
+                return RedirectToAction("Index");
+            }
+            return View(calevntVM);
+        }
+
+
+    }
+}
---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Models/Calender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PersonnelManagmentSystemV1.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonnelManagmentSystemV1.Models
+{
+    public class Calender
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+        public virtual Department Department { get; set; }
+
+        [StringLength(25)]
+        public string CalTitle { get; set; }
+
+        [StringLength(200)]
+        public string CalContent { get; set; }
+
+        public DateTime CalenderStart { get; set; }
+
+        public DateTime CalenderEnd { get; set; }
+
+
+    }
+}
